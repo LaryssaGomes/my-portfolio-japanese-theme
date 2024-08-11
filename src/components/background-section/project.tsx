3e4dee762cd9d39b672050372977c13e1cce92bf
Laryssa Gomes <laryssag533@gmail.com>
2024-08-10 23:04:30 -0300

    feat: add new projetc

--- src/components/background-section/project.tsx
+++ src/components/background-section/project.tsx
@@ -14,20 +14,15 @@ export const Project = () => {
 
       <div className="cards">
         <CardProject
-          title={"Titulo"}
-          description="Sou uma desenvolvedora full stack Jr apaixonada por criar
-                  soluções inovadoras e eficientes para problemas reais.
-                  Atualmente, trabalho na Omega Energia, uma empresa que atua no
-                  setor de energia renovável, onde utilizo React Hooks, Nest.js,
-                  Next, TypeScript, Node e styled-com ...Sou uma desenvolvedora
-                  full stack Jr apaixonada por criar soluções inovadoras e
-                  eficientes para problemas reais. Atualmente, trabalho na Omega
-                  Energia, uma empresa que atua no setor de energia"
+          title={"Nextjs-bird"}
+          description="Nextjs-bird é um site dinâmico desenvolvido com Next.js, usando dados extraídos de dois sites via Puppeteer e organizados em um CMS. O projeto inclui uma galeria de fotos históricas, integração com APIs externas, e uma ferramenta de pesquisa para identificação de aves. Todo o design da aplicação foi criado para oferecer uma experiência intuitiva e envolvente."
           tags={[
             { name: "Next", color: "#81CD39" },
             { name: "Styles-component", color: "#202147" },
+            { name: "Figma", color: "#f35c5c" },
+            { name: "Puppeteer", color: "#5415ac" },
           ]}
-          infoLink={""}
+          infoLink={"https://github.com/LaryssaGomes/Nextjs-bird"}
           projectLink={"https://nextjs-bird-exfhmmgon-laryssagomes.vercel.app/"}
           image="/imagens/projetos/bird.png"
         />
@@ -53,8 +48,8 @@ export const Project = () => {
           title="Clone Netflix"
           description="Netflix Clone é uma aplicação web desenvolvida com React.js que recria a interface visual da página inicial da Netflix. O objetivo do projeto é oferecer uma experiência visual semelhante à plataforma de streaming, utilizando dados fornecidos pela API TMDB. A aplicação exibe uma variedade de filmes e séries de TV, organizados em categorias como 'Populares' e 'Recomendados'. A interface é responsiva, garantindo uma navegação fluida em diferentes dispositivos, desde desktops até smartphones."
           tags={[
-            { name: "React", color: "#81CD39" },
-            { name: "Css", color: "#202147" },
+            { name: "React", color: "#cc34da" },
+            { name: "Css", color: "#bb5418" },
           ]}
           infoLink={"https://github.com/LaryssaGomes/clone-netflix"}
           projectLink={
