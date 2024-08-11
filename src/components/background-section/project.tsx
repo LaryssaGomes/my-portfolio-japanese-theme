11cdfff8cf598d387a957c56063380c53dfaf422
Laryssa Gomes <laryssag533@gmail.com>
2024-08-10 22:48:56 -0300

    feat: add new projetc

--- src/components/background-section/project.tsx
+++ src/components/background-section/project.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./project.styles.scss";
-import projeto from "./Captura.png";
 import { CardProject } from "@/components/cards/card-project";
 import { Tag } from "../Tag/Tags";
 
@@ -49,6 +48,19 @@ export const Project = () => {
           infoLink={""}
           projectLink={"https://desafio-iota.vercel.app/"}
           image="/imagens/projetos/lp_ponto_saber.png"
+        />
+        <CardProject
+          title="Clone Netflix"
+          description="Netflix Clone é uma aplicação web desenvolvida com React.js que recria a interface visual da página inicial da Netflix. O objetivo do projeto é oferecer uma experiência visual semelhante à plataforma de streaming, utilizando dados fornecidos pela API TMDB. A aplicação exibe uma variedade de filmes e séries de TV, organizados em categorias como 'Populares' e 'Recomendados'. A interface é responsiva, garantindo uma navegação fluida em diferentes dispositivos, desde desktops até smartphones."
+          tags={[
+            { name: "React", color: "#81CD39" },
+            { name: "Css", color: "#202147" },
+          ]}
+          infoLink={"https://github.com/LaryssaGomes/clone-netflix"}
+          projectLink={
+            "https://clone-netflix-o0dvwzxh2-laryssagomes-projects.vercel.app/watch/239526"
+          }
+          image="/imagens/projetos/netlfix_clone.png"
         />
       </div>
     </section>
